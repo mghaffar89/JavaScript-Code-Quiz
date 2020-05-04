64fe9ec4c9dbf785f4ca094c24cecead890f9cc4
Mina G <mghaffar89@gmail.com>
2020-05-04 04:16:36 -0500

    declaring all initial variables

--- script.js
+++ script.js
@@ -32,3 +32,10 @@ var questions = [
     answer: "console log",
   },
 ];
+
+var score = 0;
+var questionList = 0;
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTime");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
